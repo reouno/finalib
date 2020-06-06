5ee8bcce4cb666517d3c1c65b5ea926a5f9e3e4e
TMT <tmt@example.com>
2020-06-06 03:44:37 -0400

    :ok_hand: follow the python module style

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='finalib',
+      version='0.0.1',
+      description='Python finance utilities',
+      author='UNO Leo',
+      author_email='leouno12@gmail.com',
+      url='https://github.com/tmtlu/finalib',
+      packages=['finalib'],
+     )
