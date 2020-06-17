3f3484a37855b2a684bf045e963d96d2dc8d8164
TMT <tmt@example.com>
2020-06-16 23:37:17 -0400

    :bookmark: bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='finalib',
-      version='0.0.2',
+      version='0.1.0',
       description='Python finance utilities',
       author='UNO Leo',
       author_email='leouno12@gmail.com',
